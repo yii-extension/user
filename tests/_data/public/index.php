0a5c06218a75bc6a53727b34f44c38e2ac4aa998
Wilmer Arambula <terabytesoftw@gmail.com>
2021-01-01 12:36:18 -0300

    Fix index.php.

--- tests/_data/public/index.php
+++ tests/_data/public/index.php
@@ -8,7 +8,6 @@ use Yiisoft\Di\Container;
 use Yiisoft\ErrorHandler\ErrorHandler;
 use Yiisoft\ErrorHandler\HtmlRenderer;
 use Yiisoft\ErrorHandler\ThrowableRendererInterface;
-use Yiisoft\Files\FileHelper;
 use Yiisoft\Http\Method;
 use Yiisoft\Yii\Web\Application;
 use Yiisoft\Yii\Web\SapiEmitter;
@@ -34,12 +33,7 @@ if (PHP_SAPI === 'cli-server') {
 require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
 
 // Don't do it in production, assembling takes it's time
-$configTime = FileHelper::lastModifiedTime(dirname(__DIR__) . '/config/');
-$buildTime = FileHelper::lastModifiedTime(dirname(__DIR__) . '/runtime/build/config/');
-if ($buildTime < $configTime) {
-    Builder::rebuild();
-}
-
+Builder::rebuild();
 
 $startTime = microtime(true);
 
