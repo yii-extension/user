e8af968023e1d877b613ce61a9f86a1d14ae433b
Иван <68641750+Mister-42@users.noreply.github.com>
2021-01-08 17:31:27 +0100

    Clean config

--- config/web/repository.php
+++ config/web/repository.php
@@ -4,24 +4,13 @@ declare(strict_types=1);
 
 namespace Yii\Component;
 
-use LasseRafn\InitialAvatarGenerator\InitialAvatar;
-use Psr\Log\LoggerInterface;
 use Yii\Extension\User\Repository\RepositoryUser;
-use Yiisoft\ActiveRecord\ActiveRecordFactory;
-use Yiisoft\Aliases\Aliases;
 use Yiisoft\Auth\IdentityRepositoryInterface;
-use Yiisoft\Factory\Definitions\Reference;
 
 /** @var array $params */
 
 return [
     IdentityRepositoryInterface::class => [
         '__class' => RepositoryUser::class,
-        '__construct()' => [
-            Reference::to(ActiveRecordFactory::class),
-            Reference::to(Aliases::class),
-            Reference::to(InitialAvatar::class),
-            Reference::to(LoggerInterface::class),
-        ]
     ]
 ];
