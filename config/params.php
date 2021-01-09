4ee1af3e62f9dab0a9c12b7094a9e4be0051732c
Wilmer Arambula <terabytesoftw@gmail.com>
2021-01-09 16:20:16 -0300

    Add the ability to specify prefix in routes.

--- config/params.php
+++ config/params.php
@@ -3,17 +3,23 @@
 declare(strict_types=1);
 
 return [
+    'user' => [
+        'router' => [
+            'prefix' => null,
+        ],
+    ],
+
     'yiisoft/aliases' => [
         'aliases' => [
             '@avatars' => '@assets/images/avatar',
             '@user' => dirname(__DIR__),
-        ]
+        ],
     ],
 
     'yiisoft/yii-db-migration' => [
         'createNamespace' => 'Yii\\Extension\\User\\Migration',
         'updateNamespace' => [
             'Yii\\Extension\\User\\Migration',
-        ]
+        ],
     ],
 ];
--- config/routes.php
+++ config/routes.php
@@ -11,31 +11,40 @@ use Yii\Extension\User\Action\Recovery\Reset;
 use Yii\Extension\User\Action\Registration\Confirm;
 use Yii\Extension\User\Action\Registration\Register;
 use Yiisoft\Auth\Middleware\Authentication;
+use Yiisoft\Composer\Config\Builder;
 use Yii\Extension\User\Middleware\Guest;
+use Yiisoft\Router\Group;
 use Yiisoft\Router\Route;
 
+$params = require Builder::path('params');
+
 return [
-    Route::get('/confirm[/{id}/{code}]', [Confirm::class, 'run'])
-        ->name('confirm')
-        ->addMiddleware(Guest::class),
-    Route::methods(['GET', 'POST'], '/login', [Login::class, 'run'])
-        ->name('login')
-        ->addMiddleware(Guest::class),
-    Route::post('/logout', [Logout::class, 'run'])
-        ->name('logout'),
-    Route::methods(['GET', 'POST'], '/profile', [Profile::class, 'run'])
-        ->name('profile')
-        ->addMiddleware(Authentication::class),
-    Route::methods(['GET', 'POST'], '/request', [Request::class, 'run'])
-        ->name('request')
-        ->addMiddleware(Guest::class),
-    Route::methods(['GET', 'POST'], '/register', [Register::class, 'run'])
-        ->name('register')
-        ->addMiddleware(Guest::class),
-    Route::methods(['GET', 'POST'], '/resend', [Resend::class, 'run'])
-        ->name('resend')
-        ->addMiddleware(Guest::class),
-    Route::methods(['GET', 'POST'], '/reset[/{id}/{code}]', [Reset::class, 'run'])
-        ->name('reset')
-        ->addMiddleware(Guest::class),
+    Group::create(
+        $params['user']['router']['prefix'],
+        [
+            Route::get('/confirm[/{id}/{code}]', [Confirm::class, 'run'])
+                ->name('confirm')
+                ->addMiddleware(Guest::class),
+            Route::methods(['GET', 'POST'], '/login', [Login::class, 'run'])
+                ->name('login')
+                ->addMiddleware(Guest::class),
+            Route::post('/logout', [Logout::class, 'run'])
+                ->name('logout'),
+            Route::methods(['GET', 'POST'], '/profile', [Profile::class, 'run'])
+                ->name('profile')
+                ->addMiddleware(Authentication::class),
+            Route::methods(['GET', 'POST'], '/request', [Request::class, 'run'])
+                ->name('request')
+                ->addMiddleware(Guest::class),
+            Route::methods(['GET', 'POST'], '/register', [Register::class, 'run'])
+                ->name('register')
+                ->addMiddleware(Guest::class),
+            Route::methods(['GET', 'POST'], '/resend', [Resend::class, 'run'])
+                ->name('resend')
+                ->addMiddleware(Guest::class),
+            Route::methods(['GET', 'POST'], '/reset[/{id}/{code}]', [Reset::class, 'run'])
+                ->name('reset')
+                ->addMiddleware(Guest::class),
+        ],
+    ),
 ];
