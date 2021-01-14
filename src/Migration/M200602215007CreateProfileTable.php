3a1a595cc05bbdaeb0c5aca028751e75ebfeeeae
Иван <68641750+Mister-42@users.noreply.github.com>
2021-01-14 20:25:41 +0100

    Do not force COLLATE (#13)

--- src/Migration/M200602215007CreateProfileTable.php
+++ src/Migration/M200602215007CreateProfileTable.php
@@ -17,7 +17,7 @@ final class M200602215007CreateProfileTable implements RevertibleMigrationInterf
         $tableOptions = null;
 
         if ($b->getDb()->getDriverName() === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8mb4 ENGINE=InnoDB';
         }
 
         $b->createTable(
