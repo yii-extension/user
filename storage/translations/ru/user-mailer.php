70cedfcebd8fbabd4c3c5e8fab1bf5b2292ade2d
Wilmer <terabytesoftw@gmail.com>
2021-05-06 15:52:45 -0400

    Add russian and german language translations.

---
+++ storage/translations/ru/user-mailer.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Hello, {username},' => 'Здравствуйте, {username},',
+    'If you cannot click the link, please try pasting the text into your browser.' => 'Если вы не можете перейти по ссылке, попробуйте вставить её текст в адресную строку своего браузера.',
+    'If you did not make this request you can ignore this email.' => 'Если вы не отправляли этот запрос, просто проигнорируйте это письмо.',
+    'In order to complete your request, please click the link below:' => 'Чтобы завершить запрос, нажмите на ссылку ниже:',
+    'Thank you for signing up on {moduleName}.' => 'Благодарим вас за регистрацию в {moduleName}.',
+    'We have generated a password for you:' => 'Мы создали для вас пароль:',
+    'We have received a request to change the email address for your account on {moduleName}.' => 'Мы получили запрос на изменение адреса электронной почты для вашего аккаунта в {moduleName}.',
+    'We have received a request to reset the password for your account on {moduleName}.' => 'Мы получили запрос на сброс пароля для вашего аккаунта в {moduleName}.',
+    'Your account on {moduleName} has a new password.' => 'У вашей учетной записи в {moduleName} новый пароль.',
+    'Your account on {moduleName} has been created.' => 'Ваша учетная запись в {moduleName} создана.',
+];
