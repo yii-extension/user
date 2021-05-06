2e3ece60448e04946ece37ec80f80da4a9849139
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2021-05-06 15:59:31 -0400

    Add russian and german language translations. (#32)
    Add Russian and German language translations.

---
+++ storage/translations/de/user-view.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Already registered - Sign in!' => 'Уже зарегистрированы - Войти!',
+    'Change email address' => 'Изменить адрес электронной почты',
+    'Continue' => 'Продолжить',
+    "Didn't receive confirmation message" => "Не получили подтверждающее сообщение",
+    "Don't have an account - Sign up!" => 'Нет учетной записи - Зарегистрируйтесь!',
+    'Forgot password' => 'Забыли пароль',
+    'Log in' => 'Вход',
+    'Profile' => 'Профиль',
+    'Register' => 'Регистрация',
+    'Remember me' => 'Запомни меня',
+    'Request your password' => 'Запросить пароль',
+    'Resend confirmation message' => 'Отправить сообщение с подтверждением еще раз',
+    'Reset your password' => 'Сброс пароля',
+    'Save' => 'Сохранить',
+];
